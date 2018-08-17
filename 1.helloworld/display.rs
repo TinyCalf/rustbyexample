c2b62218236bfd472d866ab94b94c53ce5f7f147
Jonathan <839560084@qq.com>
2018-08-17 16:22:40 +0800

    lesson 2

--- 1.helloworld/display.rs
+++ 1.helloworld/display.rs
@@ -1,62 +1,11 @@
 use std::fmt;
 
-#[derive(Debug)]
-struct MinMax(i64, i64);
+struct Structure(!32);
 
-impl fmt::Display for MinMax {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({},{})", self.0, self.1)
+impl fmt::Display for Structure {
+    fn fnt($self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
     }
-}
-
-#[derive(Debug)]
-struct Point2D {
-    x: f64, 
-    y: f64,
-}
-
-
-impl fmt::Display for Point2D {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "x: {}, y: {}", self.x, self.y)
-    }
-}
-
-//Activity 
-#[derive(Debug)]
-struct Point_2D {
-     real:  f64,
-     imag:  f64,
-}
-
-impl fmt::Display for Point_2D {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} +{}i", self.real, self.imag)
-    }
-}
-
-fn main() {
-    let minmax = MinMax(0, 14);
-
-    println!("Compare sturctures:");
-    println!("Display: {}", minmax);
-    println!("Debug: {:?}", minmax);
-
-    let big_range = MinMax(-300, 300);
-    let small_range = MinMax(-3, 3);
-    println!("The big range is {big} and the small is {small}", 
-             small = small_range,
-             big = big_range);
-    let point = Point2D {x: 3.3, y: 7.2};
-  
-    println!("Compare points:");
-    println!("Display: {}", point);
-    println!("Debug: {:?}", point);
-
-    let point_2d = Point_2D { real: 3.3, imag: 7.2} ;
-    println!("Point_2D");
-    println!("Display: {}", point_2d);
-    println!("Debug: {:?}", point_2d);
 
 
 }
---
+++ 2.Primitives/2_2.rs
@@ -0,0 +1,38 @@
+use std::fmt;
+
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+
+    let (integer, boolean) = pair;
+
+    (boolean, integer)
+}
+
+
+#[derive(Display)]
+struct Matrix(f32, f32, f32, f32);
+
+fn main() {
+    
+    let tuple_of_tuples = ( (1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+    
+    println!("tuple of tuples: {:?}" , tuple_of_tuples);
+
+    let pair = (1, true);
+
+    println!("pair is {:?}", pair);
+
+    println!("the reversed pair is {:?}", reverse(pair));
+
+    println!("one element tuple: {:?}", (5u32,));
+
+    println!("just an integer: {:?}", (5u32));
+
+    let tuple = (1, "hello", 4.5, true);
+
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix);
+
+}
